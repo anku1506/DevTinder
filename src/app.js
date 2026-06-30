6f6a910a0fdb9139bd5e33f0aec4a083ec18eded
anku1506 <kumaranku946@gmail.com>
2026-07-01 03:52:59 +0530

    first routing commit

--- src/app.js
+++ src/app.js
@@ -6,17 +6,35 @@ const app = express();
 // }) - here it will be visible to all route
 
 
+
+
+// app.use("/test",(req,res) => {
+//     res.send("Hello from test2 new test");
+// })
+
+// app.use("/hello",(req,res) => {
+//     res.send("Hello from server");
+// })
+
+
+// its wildcard route
 // app.use("/",(req,res) => {
 //     res.send("from home ");
 // })
 
-app.use("/test",(req,res) => {
-    res.send("Hello from test2 new test");
+
+app.get("/user", (req, res) => {
+    res.send("Data Getting.......");
+    
 })
 
-app.use("/hello",(req,res) => {
-    res.send("Hello from server");
+app.post("/user", (req, res) => {
+    res.send("User response saved via post response");
+    
 })
+
+
+
 
 
 
