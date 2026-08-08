77fca24ffd6915fa5b787bb3004aff3bd35fe492
anku1506 <kumaranku946@gmail.com>
2026-08-09 01:01:45 +0530

    dynamic api call

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const connectDB=require("./config/database");  // needs to call the  db 
 const app = express();
-const User=require("./models/user")
+const User = require("./models/user")
+
+
+//adding json middleware
+app.use(express.json()); // this json is provided by express framework to read json convert into js object
 
 //creating a post api
 app.post("/signup", async (req, res) => {
-  
+    console.log("show payload ", req.body);  
 //creating a new instance of a user model
-    const user = new User({
-        firstName: "Satyam",
-        lastName: "kumar",
-        emailId: "satyam@gmail.com",
-        password:"sat@123"
-        
-    });
+    const user = new User(req.body);
     //save is a function which return promise
     try {
          await user.save();
     res.send("user added successfully");
         
-    } catch(err) {
+    }catch(err) {
         res.status(400).send("Error saving user" + err.message);
-    }
-   
+    }   
     
 })
 
